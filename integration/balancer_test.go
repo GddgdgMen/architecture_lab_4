23041723e313dc3e7a2550e2303b8fed51a963dc
Serhii Tkachuk <sergiytkachuk29@gmail.com>
2023-06-02 23:48:20 +0300

    Added integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 const baseAddress = "http://balancer:8090"
@@ -14,19 +17,48 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
-	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
-		t.Skip("Integration test is not enabled")
-	}
-
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+type IntegrationTestSuite struct {
+	suite.Suite
 }
 
-func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+func TestBalancer(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (s *IntegrationTestSuite) TestGetRequest() {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		s.T().Skip("Integration test is not enabled")
+	}
+
+	servers := []string{"server1:8080", "server2:8080", "server3:8080"}
+	serverCount := len(servers)
+
+	for i := 0; i < 10; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		assert.NoError(s.T(), err)
+
+		expectedServer := servers[i%serverCount]
+		assert.Equal(s.T(), expectedServer, resp.Header.Get("lb-from"))
+	}
+}
+
+func (s *IntegrationTestSuite) BenchmarkBalancer(b *testing.B) {
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		s.T().Skip("Integration test is not enabled")
+	}
+
+	b.ResetTimer()
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/some-data", baseAddress), nil)
+	assert.NoError(s.T(), err)
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			resp, err := client.Do(req)
+			assert.NoError(s.T(), err)
+			resp.Body.Close()
+		}
+	})
+
+	b.StopTimer()
 }
